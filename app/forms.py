c81ba8abaa60acde2296170b36b6e269aafeb917
ddelarosae <ddelarose@uninorte.edu.co>
2022-09-30 09:08:54 -0500

    ajustes

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import Form, StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class FormInicio(FlaskForm):
-    usuario = StringField('Usuario', validators=[DataRequired(message=‘No dejar vacío, completar')])
-    contraseña = PasswordField('Contraseña', validators=[DataRequired(message=‘No dejar vacío, completar')])
-    recordar = BooleanField('Recordar Usuario’)
+    usuario = StringField('Usuario', validators=[DataRequired(message='No dejar vacío, completar')])
+    contraseña = PasswordField('Contraseña', validators=[DataRequired(message='No dejar vacío, completar')])
+    recordar = BooleanField('Recordar Usuario')
     enviar = SubmitField('Iniciar Sesión')
